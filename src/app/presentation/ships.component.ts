056cd12b0127974133f8da241999af9359239af1
hapi <hapi@hapiware.com>
2020-06-27 21:20:21 +0300

    Calculated values cannot be selected if screen size is less or equal than 360 pixels.

--- src/app/presentation/ships.component.ts
+++ src/app/presentation/ships.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, HostListener } from '@angular/core';
 
 @Component({
     selector: 'ships',
     templateUrl: './ships.component.html'
 })
-export class ShipsComponent 
+export class ShipsComponent implements AfterViewInit
 {
     public _calculated: number = 0;
     public _shopOpen: boolean = false;
+    public _disableCalculatedValuesCheckbox: boolean = false;
 
+    public ngAfterViewInit(): void
+    {
+        this.detectScreenSize();
+    }
+
+    @HostListener("window:resize", [])
+    private onResize()
+    {
+        this.detectScreenSize();
+    }    
     public openShop()
     {
         this._shopOpen = true;
+    }
+
+    private detectScreenSize()
+    {
+        let width: number = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        this._disableCalculatedValuesCheckbox = width <= 360;
     }
 }
